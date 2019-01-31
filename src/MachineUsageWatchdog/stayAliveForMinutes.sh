b77b2b19faa8afac8b2d2a9bf74feeaf2f2d892d
Nishant Sharma <1043456+nishantsharma@users.noreply.github.com>
2019-01-31 11:35:48 +0530

    Removed MachineUsageWatchdog and moved DeepInXml to the top.

--- src/MachineUsageWatchdog/stayAliveForMinutes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-MINUTES_LEFT=$1
-until [  $MINUTES_LEFT -le 0 ]; do
-    echo Reserved minutes left are $MINUTES_LEFT
-    let MINUTES_LEFT=MINUTES_LEFT-1 
-    sleep 60
-    touch /var/run/MachineUsageWatchdog  
-done
